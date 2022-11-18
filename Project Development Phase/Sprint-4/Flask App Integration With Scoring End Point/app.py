ffcf1e5b7d93abf6b8bb304ec524662312585fe8
Shameem Faathima <71258384+shameem130@users.noreply.github.com>
2022-11-18 21:57:53 +0530

    Update app.py

--- Project Development Phase/Sprint-4/Flask App Integration With Scoring End Point/app.py
+++ Project Development Phase/Sprint-4/Flask App Integration With Scoring End Point/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 import requests
 
- 
+
 # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
 API_KEY = "S0ahhsqevpUY0Eu1YKv5Kyl38OMCy3haa5WCXw0am_wL"
 token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
